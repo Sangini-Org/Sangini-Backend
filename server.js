73a26af219f84001c7652851aae2fb9da93ace0d
Devansh Chaubey <devkumar5436@gmail.com>
2021-04-16 00:57:58 +0530

    process.env test

--- server.js
+++ server.js
@@ -17,15 +17,18 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// database
-const db = require("./app/models");
+console.log(process.env.NODE_ENV)
 
-// db.sequelize.sync();
-// db.sequelize.sync({alter: true});
+const db = require("./app/models");
+if (process.env.NODE_ENV !== "test" ){
+  // db.sequelize.sync();
+  db.sequelize.sync({alter: true});
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Database with { force: true }');
 // });
+}
+
 
 // simple route
 app.get("/", (req, res) => {
