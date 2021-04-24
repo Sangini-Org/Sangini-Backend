bad21ab3cac7984b7b4b976a18a017cb44cb03db
ranjit <ranjitmaheshshah@gmail.com>
2021-04-24 13:59:12 +0530

    chore: refactor

--- app/controllers/userImage.controller.js
+++ app/controllers/userImage.controller.js
@@ -3,17 +3,18 @@ const { cloudinary } = require('../utils/cloudinary');
 const { sendJSONResponse, sendBadRequest } = require("../utils/handle")
 
 const userImage = db.userImages;
+const Op = db.Sequelize.Op;
 
 exports.addUserImage = async (req, res) => {
   try {
     const { type } = req.body;
     const file = req.files.image;
-    await cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
+    await cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
       if (err) {
         return sendBadRequest(res, 404, 'Error while uploading file to cloudinary' + err);
       }
       else {
-        const image = await userImage.create({
+         userImage.create({
           publicId: result.public_id,
           url: result.secure_url,
           imgType: type,
@@ -31,13 +32,16 @@ exports.addUserImage = async (req, res) => {
 exports.getUserImage = async (req, res) => {
   try {
     const { type } = req.query;
+    let condition = {};
+    condition.userId = req.userId;
+    if(type){
+      condition.imgType = type;
+    }
     const images = await userImage.findAll({
-      where: {
-        imgType: type
-      }
+      where: condition
     })
     if (images) {
-      res.send(images);
+      return sendJSONResponse(res, 200,"user images", images)
     } else {
       return sendBadRequest(res, 404, "Users Not Found");
     }
@@ -51,15 +55,18 @@ exports.updateUserImage = async (req, res) => {
   try {
     const { publicId } = req.body;
     const file = req.files.image;
-    await cloudinary.uploader.upload(file.tempFilePath, { public_id: publicId }, async (err, result) => {
+    await cloudinary.uploader.upload(file.tempFilePath, { public_id: publicId },  (err, result) => {
       if (err) {
         return sendBadRequest(res, 404, 'Error while updating file to cloudinary' + err);
       }
       else {
-        const image = await userImage.update({
+        userImage.update({
           url: result.secure_url,
         }, {
-          where: { publicId: publicId }
+          where: { 
+            publicId: publicId,
+            userId: req.userId
+           }
         });
       }
     });
