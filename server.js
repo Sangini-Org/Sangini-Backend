f18674d8f2b22fb4651687257100cbbeba0ed1a8
Harsh Singh <harshaiactr@gmail.com>
2021-05-04 21:58:45 +0530

    resolved conflicts

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 if (process.env.NODE_ENV !== "test" ){
-   db.sequelize.sync();
+ db.sequelize.sync();
 // db.sequelize.sync({alter: true});
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
