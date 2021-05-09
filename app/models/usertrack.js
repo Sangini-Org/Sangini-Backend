a0eb94880c168a92b9bac5201a190ac6b965012d
ranjit <ranjitmaheshshah@gmail.com>
2021-05-10 01:23:52 +0530

    added artist data in usertrack model

--- app/models/usertrack.js
+++ app/models/usertrack.js
@@ -1,9 +1,15 @@
 const { Sequelize, DataTypes, Model } = require("sequelize");
 
 module.exports = (sequelize, Sequelize) => {
-  const usertrack = sequelize.define("usertracks", {  
-    tracklist:{
-        type:Sequelize.ARRAY(Sequelize.STRING)
+  const usertrack = sequelize.define("usertracks", {
+    tracklist: {
+      type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    artistList: {
+      type: Sequelize.ARRAY(Sequelize.STRING)
+    },
+    artistCount: {
+      type: Sequelize.INTEGER
     }
   });
 
