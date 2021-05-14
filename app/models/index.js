0022151ec387b91b2dbaaea2ebb844fac0f5732b
harshsingh25 <66053605+harshsingh25@users.noreply.github.com>
2021-05-14 20:38:38 +0530

    User Status model (#23)
    * added status model
    
    * added search by username & default sorting by like and updatedAt
    
    * added disliking route which unlike the like count
    
    * Update userStatus.controller.js
    
    * Update app/controllers/userStatus.controller.js
    
    * Update app/controllers/userStatus.controller.js
    
    * Update app/controllers/userStatus.controller.js
    
    * Update app/controllers/userStatus.controller.js
    
    * allusername with similar word searches and not found case handle
    
    * my changes
    
    * like status update
    
    * likeCount added
    
    Co-authored-by: ranjit shah <ranjitmaheshshah@gmail.com>

--- app/models/index.js
+++ app/models/index.js
@@ -36,6 +36,7 @@ db.users.hasMany(db.userImages);
 db.userImages.belongsTo(db.users);
 db.users.hasOne(db.usertracks);
 db.usertracks.belongsTo(db.users);
-
+db.users.hasOne(db.userStatus);
+db.userStatus.belongsTo(db.users);
 
 module.exports = db;
