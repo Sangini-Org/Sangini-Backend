5de295d57638f502eff6c593def15f311f04ed30
harshsingh25 <66053605+harshsingh25@users.noreply.github.com>
2021-06-10 00:45:03 +0530

    Userprofileupdate state (#33)
    * update state
    
    * codacy fix
    
    * integer to string
    
    Co-authored-by: ranjit <ranjitmaheshshah@gmail.com>

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const db = require("./app/models");
 if (process.env.NODE_ENV !== "test" ){
 
- db.sequelize.sync();
-// db.sequelize.sync({alter: true});
+//  db.sequelize.sync();
+db.sequelize.sync({alter: true});
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 // console.log('Drop and Resync Database with { force: true }');
